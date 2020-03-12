7784c0f0e76b78d8f968cb3c7aa01037fc671024
qpc2015 <17771869676@163.com>
2020-03-12 23:43:35 +0800

    修改webview加载方案

--- lib/pages/Product/ProductSecond.dart
+++ lib/pages/Product/ProductSecond.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import '../../model/ProductContentMode.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 class ProductSecondPage extends StatefulWidget {
   final ProductContentItem _item;
-  ProductSecondPage(this._item,{Key key}) : super(key: key);
+  ProductSecondPage(this._item, {Key key}) : super(key: key);
 
   @override
   _ProductSecondPageState createState() => _ProductSecondPageState();
@@ -13,19 +13,14 @@ class ProductSecondPage extends StatefulWidget {
 class _ProductSecondPageState extends State<ProductSecondPage> {
   @override
   Widget build(BuildContext context) {
+    print("http://jd.itying.com/pcontent?id=${widget._item.sId}");
     return Container(
-       child: Column(
-         children: <Widget>[
-           Expanded(
-             child: InAppBrowser(
-               initialUrl:"http://jd.itying.com/pcontent?id=${widget._item.sId}",
-               onProgressChanged: (InAppWebViewController controller, int progress) {
-                    print(progress);
-                  }
-           )
-           )
-         ],
-       ),
+      child: Column(
+        children: <Widget>[
+          Expanded(
+              child: WebviewScaffold(url:"http://jd.itying.com/pcontent?id=${widget._item.sId}"))
+        ],
+      ),
     );
   }
 }
